ed237541c0995ccf1458cdde17387640ec6d5ebe
Just824 <156043990+Just824@users.noreply.github.com>
2024-12-13 11:23:47 +0800

    Update card.vue

--- docs/examples/carousel/card.vue
+++ docs/examples/carousel/card.vue
@@ -1,10 +1,19 @@
 <template>
   <el-carousel :interval="4000" type="card" height="200px">
     <el-carousel-item v-for="item in 6" :key="item">
-      <h3 text="2xl" justify="center">{{ item }}</h3>
+      <img :src="item" alt="轮播图片" style="width: 100%; height: 100%; object-fit: cover;">
     </el-carousel-item>
   </el-carousel>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import image1 from '@/assets/images/image1.jpg';
+import image2 from '@/assets/images/image2.jpg';
+import image3 from '@/assets/images/image3.jpg';
+
+const images = ref([image1, image2, image3]);
+</script>
 
 <style scoped>
 .el-carousel__item h3 {
